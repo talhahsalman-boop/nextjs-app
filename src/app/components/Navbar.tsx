8350bc124afc6b620ee514281b66269e618f8bf4
Talha Salman <talhahsalman-boop>
2026-05-15 12:44:13 +0500

    resolve merge conflict

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,14 +4,14 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-      <nav className='Navbr w-[92vw] h-18 mb-8 mt-5 bg-red-400 flex justify-around items-center ml-15 rounded-2xl'>
+      <nav className='Navbr w-[92vw] h-18 mb-8 mt-5 bg-red-600 flex justify-around items-center ml-15 rounded-2xl'>
         <div className="logo text-[22px] font-bold text-white">Apna Card</div>
         <div className="links ">
             <ul className='flex gap-3'>
-                <li className='text-[18px]'> 
+                <li className='text-[18px] text-white'> 
                     <Link href="/"> Read</Link>
                     </li>
-                <li className='text-[18px]'>
+                <li className='text-[18px] text-white'>
                      <Link href= "/write"> Write</Link>
                      </li>
             </ul>
